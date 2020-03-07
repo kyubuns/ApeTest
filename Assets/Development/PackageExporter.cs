4ebf3a9ba7968f555f74abed0a81e77800535a82
kyubuns <kyubuns@gmail.com>
2020-03-08 00:04:42 +0900

    feat: パッケージ作りテンプレ

---
+++ Assets/Development/PackageExporter.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEditor;
+
+namespace Development
+{
+    public static class PackageExporter
+    {
+        [MenuItem("Export/Export Unity Package")]
+        public static void ExportUnityPackage()
+        {
+            var libraryDirectories = new[]
+            {
+                "Assets/ApeTest"
+            };
+
+            var outputFilePath = Path.Combine(Path.GetDirectoryName(Application.dataPath), "ApeTest.unitypackage");
+
+            Debug.LogFormat("Export package to {0}", outputFilePath);
+            AssetDatabase.ExportPackage(libraryDirectories, outputFilePath, ExportPackageOptions.Recurse);
+            Debug.LogFormat("Success");
+        }
+    }
+}
